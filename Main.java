b84cb158c6280023e46f6e80aca8497bfee8d232
AmitRai670 <8387amit@gmail.com>
2025-09-27 15:48:47 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
-package OOPs.SataticExample;
+package OOPs.inheritance;
 
 public class Main {
-    public static void main(String[] args){
-        Human Amit = new Human(22,"Amit",10000,false);
-        Human Sujit= new Human (19,"Sujit",50000,true);
+    public static void main(String[] args) {
+//        box Box1 = new box();
 
-        System.out.println(Amit.population);
-        System.out.println(Sujit.population);
+//        System.out.println(Box1.l+" "+Box1.h+" "+Box1.w);
 
+        Boxweight box2 =new Boxweight();
+        Boxweight box3 =new Boxweight(2,3,4,5);
+
+        System.out.println(box2.h+" "+ box3.weight);
     }
 }
