c78ba70e2be05b1e81e9a82320df63123cf5fce6
AmitRai670 <8387amit@gmail.com>
2025-09-29 00:30:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,10 +5,13 @@ public class Main {
 //        box Box1 = new box();
 
 //        System.out.println(Box1.l+" "+Box1.h+" "+Box1.w);
+//
+//        Boxweight box2 =new Boxweight();
+//        Boxweight box3 =new Boxweight(2,3,4,5);
+//
+//        System.out.println(box2.h+" "+ box3.weight);
 
-        Boxweight box2 =new Boxweight();
-        Boxweight box3 =new Boxweight(2,3,4,5);
-
-        System.out.println(box2.h+" "+ box3.weight);
+        BoxPrice box =new BoxPrice(7,3,200);
     }
 }
+
